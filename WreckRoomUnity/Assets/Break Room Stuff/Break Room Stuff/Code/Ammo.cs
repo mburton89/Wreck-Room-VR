61fa15dd16b35da33bab323f67d327e7aae33179
Matt Burton <matthewwburton@gmail.com>
2022-10-06 15:40:33 -0500

    Got gym in

---
+++ WreckRoomUnity/Assets/Break Room Stuff/Break Room Stuff/Code/Ammo.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour
+{
+    [SerializeField] MeshRenderer colorShift;
+    [SerializeField] Material hitColor;
+    AudioSource audioSource;
+
+    public enum ammoType
+    {
+        basic,
+        breakable,
+        reusable,
+    }
+
+    public ammoType currentAmmoType;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != "SpawnZone" && collision.gameObject.tag != "Player" && collision.gameObject.tag != "Ammo")
+        {
+            HandleImpact();
+        }
+    }
+
+    void HandleImpact()
+    {
+        if (currentAmmoType == ammoType.basic)
+        {
+            //Nothing?
+        }
+        else if (currentAmmoType == ammoType.breakable)
+        {
+            Destroy(this.gameObject);
+        }
+        else if (currentAmmoType == ammoType.reusable)
+        {
+            colorShift.material = hitColor;
+        }
+
+        //audioSource.Play();
+    }
+}
